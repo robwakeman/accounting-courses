2f0573944562739190f9a91fa7e12415927cda46
Rob Wakeman <rob@robwakeman.com>
2020-03-07 17:31:09 +0000

    attach event listeners to filter buttons, filter buttons correctly filtering data, display array length, rename all filter names to use full names

--- app/js/app.js
+++ app/js/app.js
@@ -14,9 +14,21 @@ function runApp() {
   const coursesContainer = document.querySelector('[data-courses]');
   // console.log('coursesContainer is: ', coursesContainer);
 
+  const filterAll = document.querySelector("[data-filter='all']");
+  // console.log('filterTax is: ', filterTax);
+  const filterTax = document.querySelector("[data-filter='tax']");
+  // console.log('filterTax is: ', filterTax);
+  const filterCommunication = document.querySelector("[data-filter='communication']");
+  // console.log('filterTax is: ', filterTax);
+  const filterTechnology = document.querySelector("[data-filter='technology']");
+  // console.log('filterTax is: ', filterTax);
+
   // API URL (array of 29 objects)
   const APIURL = 'https://learn.accountingcpd.net/ACPD/API/Test/SampleObject';
   let coursesAll;
+  let coursesTax;
+  let coursesCommunication;
+  let coursesTechnology;
 
   // get courses
   function getCourses() {
@@ -29,12 +41,12 @@ function runApp() {
         // save the data in variable coursesAll
         coursesAll = data;
 
-        const coursesTax = coursesAll.filter(course => course.type === 'tax');
+        coursesTax = coursesAll.filter(course => course.type === 'tax');
         console.log('coursesTax: ', coursesTax);
-        const coursesCommun = coursesAll.filter(course => course.type === 'communication');
-        console.log('coursesCommun: ', coursesCommun);
-        const coursesTech = coursesAll.filter(course => course.type === 'technology');
-        console.log('coursesTech: ', coursesTech);
+        coursesCommunication = coursesAll.filter(course => course.type === 'communication');
+        console.log('coursesCommunication: ', coursesCommunication);
+        coursesTechnology = coursesAll.filter(course => course.type === 'technology');
+        console.log('coursesTechnology: ', coursesTechnology);
 
         // console.log(coursesAll);
 
@@ -53,7 +65,7 @@ function runApp() {
   }
 
   function showOutput(outputData) {
-    let output = '<h2>Courses</h2>';
+    let output = `<h2>Courses (${outputData.length})</h2>`;
     outputData.forEach(function(course) {
       output += `
       <div">
@@ -66,4 +78,41 @@ function runApp() {
   }
 
   getCourses();
+
+  filterAll.addEventListener(
+    'click',
+    function() {
+      showOutput(coursesAll);
+    },
+    false
+  );
+
+  filterTax.addEventListener(
+    'click',
+    function() {
+      showOutput(coursesTax);
+    },
+    false
+  );
+
+  filterCommunication.addEventListener(
+    'click',
+    function() {
+      showOutput(coursesCommunication);
+    },
+    false
+  );
+
+  filterTechnology.addEventListener(
+    'click',
+    function() {
+      showOutput(coursesTechnology);
+    },
+    false
+  );
+
+  /*
+   *
+   *
+   * */
 } // END runApp()
