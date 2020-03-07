032f19ab1cb77fd08b168153d08eb4b2b2c79637
Rob Wakeman <rob@robwakeman.com>
2020-03-07 14:12:42 +0000

    fetch courses and display in basic markup just to see data

--- app/js/app.js
+++ app/js/app.js
@@ -7,4 +7,43 @@ function runApp() {
   const func = msg => console.log(msg);
 
   func('hello there called inside runApp');
-}
+
+  /* ************************** */
+
+  // get elements
+  const coursesContainer = document.querySelector('[data-courses]');
+  console.log('coursesContainer is: ', coursesContainer);
+
+  // API URL
+  const APIURL = 'https://learn.accountingcpd.net/ACPD/API/Test/SampleObject';
+
+  // get courses
+  function getCourses() {
+    fetch(APIURL)
+      .then(handleErrors)
+      .then(res => res.json())
+      .then(data => {
+        let output = '<h2>Courses</h2>';
+        data.forEach(function(course) {
+          output += `
+      <div">
+        <h3>${course.title}</h3>
+        <p>${course.type}</p>
+      </div>
+    `;
+        });
+        coursesContainer.innerHTML = output;
+      })
+      .catch(err => console.log('Catch Error', err));
+  }
+
+  // handle errors
+  function handleErrors(res) {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    return res;
+  }
+
+  getCourses();
+} // END runApp()
